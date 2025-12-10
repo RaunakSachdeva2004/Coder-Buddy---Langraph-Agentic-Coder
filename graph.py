850c12b901cc16e25d8b118971f04ce869121726
Raunak Sachdeva <raunakdachdeva2004@gmail.com>
2025-12-10 14:09:16 +0530

    add graph file

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain_groq.chat_models import ChatGroq
 from langgraph.constants import END
 from langgraph.graph import StateGraph
-from langgraph.prebuilt import create_react_agent
+# from langgraph.prebuilt import create_react_agent
 
 from agent.prompts import *
 from agent.states import *
@@ -18,23 +18,36 @@ def planner_agent(state : dict) -> dict :
     user_prompt = state["user_prompt"]
     resp = llm.with_structured_output(Plan).invoke(
         planner_prompt(user_prompt))
+    if resp is None:
+        raise ValueError("Planner did not return a valid response.")
     return {"plan": resp}
 
+
+
+def architect_agent(state : dict )-> dict :
+    plan: Plan = state["plan"]
+    resp = llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
+    if resp is None:
+        raise ValueError("Architect did not return a valid response.")
+    
+    resp.plan = plan
+    return {"task_plan" : resp}
 
 
 
 
 graph = StateGraph(dict)
 graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
+graph.add_edge("planner", "architect")
 graph.set_entry_point("planner")
 
 
 agent = graph.compile()
 
 
-user_prompt = "create a simple calculator web application"
-
-
-result = agent.invoke({"user_prompt": user_prompt})
-
-print(result)
+if __name__ == "__main__":
+    user_prompt = "create a simple calculator web application"
+    
+    result = agent.invoke({"user_prompt" : user_prompt})
+    print(result)
